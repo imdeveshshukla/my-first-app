627dc9e7467f77ab13ac76e17970b7165cb66cc6
Devesh Shukla <deveshshukla1603@gmail.com>
2023-09-05 17:27:32 +0530

    test

--- app/page.js
+++ app/page.js
@@ -1,14 +1,15 @@
 // import React from 'react';
 
-import { useState } from "react";
-
 // import { useState } from "react"0;
-
+import Profile from './profile.js'
 function firstPage() {
     const i = 10;
-    useState
+    
     return ( 
-        <div className="font-bold text-5xl" id="Hellow">Hello World {i}</div>
+        <div>
+            <div className="font-bold text-5xl" id="Hellow">Hello World {i}</div>
+            <Profile/>
+        </div>
      );
 }
 
---
+++ app/profile.js
@@ -0,0 +1,8 @@
+function Profile() {
+    return (
+        <h1>
+            This is my profile
+        </h1>
+    )
+}
+export default Profile;
